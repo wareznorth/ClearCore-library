e730797c9a2f86a0b6cb021b91a410fdcc05714d
wareznorth <wareznorth@gmail.com>
2026-04-27 11:24:56 -0600

    Use fixed A11 startup setpoint of 100 Hz

--- ProjectTemplate/main.cpp
+++ ProjectTemplate/main.cpp
@@ -89,6 +89,7 @@ static void ProximityRiseCallback();
 #define a11IntegralMax 80.0f
 #define a11ErrorDeadbandHz 4.50f
 #define a11SetpointBlendAtMin 0.45f
+#define a11StartupSetpointHz 100.0f
 
 
 // Debounce helper for a digital input.
@@ -868,7 +869,7 @@ int main(void) {
                             }
 
                             if (!a11SetpointValid) {
-                                a11SetpointHz = proxHzAvg;
+                                a11SetpointHz = a11StartupSetpointHz;
                                 a11SetpointValid = true;
                                 a11Integral = 0.0f;
                                 a11PrevError = 0.0f;
